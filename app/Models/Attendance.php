82a3020e0e3927ef4dff1b5d28c0dd412425fa26
Bryan Cortez <bancortez2@gmail.com>
2023-04-30 21:33:45 +0800

    room and attendance model fillable

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Attendance extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'room_id',
+        'section_id',
+        'subject_id',
+        'semester_id',
+        'attendance_group',
+        'attendance_status',
+        'attendance_day',
+        'attendance_start_time',
+        'attendance_end_time',
+        'attendance_comments',
+    ];
 }
